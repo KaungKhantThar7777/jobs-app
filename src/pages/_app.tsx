ea6a943ef7f905cac902cee9ba55afb2848d4db1
kaungkhantthar7777 <kaungkhantthar77@gmail.com>
2022-12-12 08:24:32 +0630

    setup eslint and prettier

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,9 @@
-import "../styles/globals.css";
-import type { AppProps } from "next/app";
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
 
-function App({ Component, pageProps }: AppProps) {
+export default function App({
+  Component,
+  pageProps,
+}: AppProps) {
   return <Component {...pageProps} />;
 }
